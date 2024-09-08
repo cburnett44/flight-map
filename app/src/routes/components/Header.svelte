5b8a7a1e497e92f106424a6f45dd06b601a218df
Connor Burnett <connorburnett273@yahoo.com>
2024-09-08 11:21:27 -0600

    I-2: made darkmode auto enabled

--- app/src/routes/components/Header.svelte
+++ app/src/routes/components/Header.svelte
@@ -3,9 +3,9 @@
     import { SearchOutline } from 'flowbite-svelte-icons';
   </script>
   
-  <Navbar>
+<Navbar>
     <NavBrand href="/">
-      <span class="self-center whitespace-nowrap text-3xl font-semibold text-flighteye">FLIGHTEYE</span>
+        <span class="self-center whitespace-nowrap text-3xl font-semibold text-flighteye">FLIGHTEYE</span>
     </NavBrand>
         <div class="flex md:order-2">
             <Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
@@ -17,6 +17,6 @@
                 </div>
                 <Input id="search-navbar" class="ps-10" placeholder="Search..." />
             </div>
-            <DarkMode class="border-gray-200 md:ml-2"/>
+            <DarkMode class="md:ml-2"/>
         </div>
-  </Navbar>
+</Navbar>
