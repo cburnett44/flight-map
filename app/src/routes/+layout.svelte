94423d4bb70547defa6fb93e6414ff5d99a94bea
Connor Burnett <connorburnett273@yahoo.com>
2024-09-08 09:41:41 -0600

    I-2: new component

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import '../app.css';
+    import Header from './components/Header.svelte';
+</script>
+
+<div class="app">
+    <Header />
+
+    <main>
+        <slot />
+    </main>
+
+    <footer>
+        <p>Powered by FlightEye</p>
+    </footer>
+</div>
