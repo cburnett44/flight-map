bdf174349d8014051d1e6c2f574fb64db81f8b0f
Connor Burnett <connorburnett273@yahoo.com>
2024-09-17 20:32:33 -0600

    I-4: code cleanup and now using live map state to query adsb

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,111 +1,139 @@
 <script lang='ts'>
-    import '@maptiler/sdk/dist/maptiler-sdk.css';
-    import { config, Map, type MapOptions, MapStyle } from '@maptiler/sdk';
-    import { onMount } from 'svelte';
-	  import { isDarkMode } from '$lib/darkModeStore.js';
-    import planeIcon from '$lib/images/icon-plane-512.png';
-	  import type { Aircraft } from '$lib/models/aircraft.js';
+  import '@maptiler/sdk/dist/maptiler-sdk.css';
+  import { config, Map, type MapOptions, MapStyle } from '@maptiler/sdk';
+  import { onMount } from 'svelte';
+  import { isDarkMode } from '$lib/darkModeStore.js';
+  import planeIcon from '$lib/images/icon-plane-512.png';
+  import type { Aircraft } from '$lib/models/aircraft.js';
+  import { distance } from '@turf/turf';
 
-    // Configure key and units.
-    export let data;
-    config.apiKey = data.apiKey;
-    config.unit = 'imperial';
+  // Configure key, map units, and subscribe to the dark mode style changer.
+  export let data;
+  config.apiKey = data.apiKey;
+  config.unit = 'imperial';
 
-    // Subscribe to the dark mode store.
-    let map: Map;
-    isDarkMode.subscribe(updateMapDarkModeStyle);
+  let map: Map;
+  isDarkMode.subscribe(updateMapDarkModeStyle);
 
-    /**
-     * Change map styling based on dark mode preferences.
-     * @param darkMode
-     */
-    function updateMapDarkModeStyle(darkMode: boolean) {
-      const style = darkMode ? MapStyle.BASIC.DARK : MapStyle.BASIC.LIGHT;
-      if (map) map.setStyle(style);
+  /**
+   * Change map styling based on dark mode preferences.
+   * @param darkMode
+   */
+  function updateMapDarkModeStyle(darkMode: boolean) {
+    const style = darkMode ? MapStyle.BASIC.DARK : MapStyle.BASIC.LIGHT;
+    if (map) map.setStyle(style);
+  }
+
+  /**
+   * Initalizes the starting map options.
+   */
+  function initMapOptions() {
+    const options: MapOptions = {
+      container: document.getElementById("map") as HTMLElement,
+      style: document.documentElement.classList.contains('dark') 
+        ? MapStyle.BASIC.DARK
+        : MapStyle.BASIC.LIGHT,
+      center: [-105.021440, 40.032666],
+      zoom: 10,
+      navigationControl: false,
+      scaleControl: 'bottom-left'
     }
+    return options;
+  }
 
-    onMount(async () => {
-      const initialDarkMode = document.documentElement.classList.contains('dark');
-      const initStyle = initialDarkMode ? MapStyle.BASIC.DARK : MapStyle.BASIC.LIGHT;
-      const options: MapOptions = {
-        container: document.getElementById("map") as HTMLElement,
-        style: initStyle,
-        center: [-105.021440, 40.032666],
-        zoom: 10,
-        navigationControl: false,
-        scaleControl: 'bottom-left'
+  /**
+   * Loads the airplane icon into the map.
+   */
+  async function loadAirplaneIcon() {
+    const response = await map.loadImage(planeIcon);
+    map.addImage('airplane', response.data);
+  }
+
+  /**
+   * Calculates the viewable area by using turf distance function.
+   */
+  function calculateMapViewingDistance() {
+    const bounds = map.getBounds();
+    const sw = bounds.getSouthWest();
+    const ne = bounds.getNorthEast();
+    return distance([sw.lng, sw.lat], [ne.lng, ne.lat], { units: 'miles'});
+  }
+
+  /**
+   * Generates the needed query params to request nearby aircraft based on the current state of the map.
+   */
+  function generateQueryParams() {
+    const viewingDistance = calculateMapViewingDistance();
+    const center = map.getCenter();
+    return new URLSearchParams({
+      lat: center.lat.toString(),
+      lon: center.lng.toString(),
+      dist: viewingDistance.toString()
+    }).toString();
+  }
+
+  /**
+   * Updates the map based on incoming ADSB data.
+   */
+  async function updateMap() {
+    // Make API request to proxy and wait a list of aircraft.
+    const apiResponse = await fetch(`/api?${generateQueryParams()}`)
+    const data = await apiResponse.json();
+    const aircrafts: Aircraft[] = data as Aircraft[];
+
+    // Add each aircraft to the map depending on its most recent data.
+    aircrafts.forEach((aircraft) => {
+      const sourceId = `${aircraft.id}`
+      const sourceData : GeoJSON.FeatureCollection = {
+        type: 'FeatureCollection',
+        features: [{
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [aircraft.lon, aircraft.lat]
+          },
+          properties: { rotation: aircraft.heading }
+        }]
       }
-      map = new Map(options);
 
-      map.on('load', async function() {
-        const response = await map.loadImage(planeIcon);
-        map.addImage('airplane', response.data);
+      // If the source already exists, update it's data. Else add the source.
+      if (map.getSource(sourceId)) {
+        (map.getSource(sourceId) as maplibregl.GeoJSONSource).setData(sourceData);
+      } 
+      else {
+        map.addSource(sourceId, {
+          type: 'geojson',
+          data: sourceData
+        });
+      }
 
-        async function updateMap() {
-          const apiResponse = await fetch('/api');
-          const data = await apiResponse.json();
-          const aircrafts: Aircraft[] = data as Aircraft[];
-          aircrafts.forEach((aircraft) => {
-            // if the source was already added, change its data.
-            if (map.getSource(`${aircraft.id}`)) {
-              (map.getSource(`${aircraft.id}`) as maplibregl.GeoJSONSource).setData({
-                'type': 'FeatureCollection',
-                'features': [
-                  {
-                    'type': 'Feature',
-                    'geometry': {
-                      'type': 'Point',
-                      'coordinates': [aircraft.lon, aircraft.lat]
-                    },
-                    'properties': {
-                      'rotation': aircraft.heading
-                    }
-                  }
-                ]
-              });
-            }
-            // if the source is new, add source to the map.
-            else {
-              map.addSource(`${aircraft.id}`, {
-                'type': 'geojson',
-                'data': {
-                  'type': 'FeatureCollection',
-                  'features': [
-                    {
-                      'type': 'Feature',
-                      'geometry': {
-                        'type': 'Point',
-                        'coordinates': [aircraft.lon, aircraft.lat]
-                      },
-                      'properties': {
-                        'rotation': aircraft.heading
-                      }
-                    }
-                  ]
-                }
-              });
-            }
-
-            // Add the layer to the map
-            map.addLayer({
-              'id': `${aircraft.id}`,
-              'type': 'symbol',
-              'source': `${aircraft.id}`,
-              'layout': {
-                'icon-image': 'airplane',
-                'icon-rotate': ['get', 'rotation'],
-                'icon-allow-overlap': true,
-                'icon-size': 0.1
-              }
-            });
-          });
-        }
-
-        await updateMap();
-        setInterval(updateMap, 1000);
-
-      });
+      if (!map.getLayer(sourceId)) {
+        map.addLayer({
+          id: sourceId,
+          type: 'symbol',
+          source: sourceId,
+          layout: {
+            'icon-image': 'airplane',
+            'icon-rotate': ['get', 'rotation'],
+            'icon-allow-overlap': true,
+            'icon-size': 0.1
+          }
+        });
+      }
     });
+  }
+
+  /**
+   * When the map is added to the dom, load in all aircraft data.
+  */
+  onMount(async () => {
+    map = new Map(initMapOptions());
+    map.on('load', async function() {
+      await loadAirplaneIcon();
+      await updateMap();
+      setInterval(updateMap, 10000);
+    });
+  });
 </script>
 
 <div class="flex justify-center items-center relative w-full h-full md:py-5 overflow-hidden">
--- app/src/routes/api/+server.ts
+++ app/src/routes/api/+server.ts
@@ -1,16 +1,20 @@
 import { json } from '@sveltejs/kit';
 import type { Aircraft } from '$lib/models/aircraft';
+import type { RequestEvent } from '../$types';
 
 /**
- * Proxy the adsb.lol endpoint so we can map live adsb data into Aircraft objects and pass to the client.
+ * Proxy the adsb.lol endpoint so we can map ADSB data to the Aircraft interface and pass to client.
  * @returns 
  */
-export async function GET(): Promise<Response>{
+export async function GET({ url }: RequestEvent): Promise<Response>{
+    const lat = parseFloat(url.searchParams.get('lat') || '0');
+    const lon = parseFloat(url.searchParams.get('lon') || '0');
+    const dist = parseInt(url.searchParams.get('dist') || '0');
+
     const headers: Headers = new Headers();
     headers.set('Content-Type', 'application/json');
     headers.set('Accept', 'application/json');
-
-    const request: RequestInfo = new Request('https://api.adsb.lol/v2/lat/39.563401/lon/-104.859232/dist/30', {
+    const request: RequestInfo = new Request(`https://api.adsb.lol/v2/lat/${lat}/lon/${lon}/dist/${dist}`, {
         method: 'GET',
         headers: headers
     });
@@ -19,16 +23,17 @@ export async function GET(): Promise<Response>{
         const reponse = await fetch(request);
         const data = await reponse.json();
         const aircrafts: Aircraft[] = [];
-
         for (let i = 0; i < data.ac.length; i++) {
             const entry = data.ac[i];
-            const aircraft: Aircraft = {
-                id: entry.hex,
-                lat: entry.lat,
-                lon: entry.lon,
-                heading: entry.track
-            };
-            aircrafts.push(aircraft);
+            if (entry.lat != undefined && entry.lon != undefined && entry.track != undefined) {
+                const aircraft: Aircraft = {
+                    id: entry.hex,
+                    lat: entry.lat,
+                    lon: entry.lon,
+                    heading: entry.track
+                };
+                aircrafts.push(aircraft);
+            }
         }
         return json(aircrafts);
     }
